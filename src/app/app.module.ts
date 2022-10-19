8851147be3da705739697493bf155caf09c7c4b4
virshu <felix@rabinovich.org>
2022-10-18 17:45:19 -0700

    Created Home, Counter and FetchData components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { CounterComponent } from './counter/counter.component';
+import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    CounterComponent,
+    HomeComponent,
+    FetchDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html',
+  styleUrls: ['./fetch-data.component.css']
+})
+export class FetchDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
